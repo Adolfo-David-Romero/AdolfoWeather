3da13b658ae869913dcf2d38878be1c7bca59ef6
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2024-11-07 12:26:43 -0500

    Initial Commit

---
+++ AdolfoWeather/AdolfoWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdolfoWeatherApp.swift
+//  AdolfoWeather
+//
+//  Created by David Romero on 2024-11-07.
+//
+
+import SwiftUI
+
+@main
+struct AdolfoWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdolfoWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AdolfoWeather
+//
+//  Created by David Romero on 2024-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
