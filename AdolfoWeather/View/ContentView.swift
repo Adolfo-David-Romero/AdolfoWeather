5b6d6f6db35f0d11522932dae2f1d88d1e658740
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2024-11-10 15:15:37 -0500

    Created Basic Viewmodel

--- AdolfoWeather/View/ContentView.swift
+++ AdolfoWeather/View/ContentView.swift
@@ -8,48 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    //
+    @State private var temp_c: String = "hello"
+    @State private var feelslike_c: String = ""
+    @State private var wind_kph: String = ""
+    @State private var wind_dir: String = ""
+    @State private var humidity: String = ""
+    @State private var uv: String = ""
+    @State private var vis_km: String = ""
+    @State private var condition: String = ""
+    @State private var city_name: String = ""
+    @State private var country_name: String = ""
     var body: some View {
+        
         NavigationStack{
             VStack {
                 List{
                     Section("Tempature Data:"){
                         Section(){
-                            Text("Tempature in Cº:").fontWeight(.semibold)
-                            Text("st").font(.subheadline).fontWeight(.light)
+                            HStack{
+                                Text("Tempature in Cº:").fontWeight(.semibold)
+                                Text("\(temp_c)").font(.subheadline).fontWeight(.light)
+                            }
                         }
-                        Section(":"){
-                            
-                            Text("st").font(.subheadline).fontWeight(.light)
+                        Section(){
+                            HStack(){
+                                Text("Tempature in Cº:").fontWeight(.semibold)
+                                Text("st").font(.subheadline).fontWeight(.light)
+                            }
                         }
-                        Section("Wind Kph:"){
-                            Text("st").font(.subheadline).fontWeight(.light)
+                        Section(){
+                            HStack(){
+                                Text("Tempature in Cº:").fontWeight(.semibold)
+                                Text("st").font(.subheadline).fontWeight(.light)
+                            }
                         }
-                        Section("Wind Direction:"){
-                            Text("st").font(.subheadline).fontWeight(.light)
+                        Section(){
+                            HStack(){
+                                Text("Tempature in Cº:").fontWeight(.semibold)
+                                Text("st").font(.subheadline).fontWeight(.light)
+                            }
                         }
-                        Section("UV:"){
-                            Text("st").font(.subheadline).fontWeight(.light)
+                        Section(){
+                            HStack(){
+                                Text("Tempature in Cº:").fontWeight(.semibold)
+                                Text("st").font(.subheadline).fontWeight(.light)
+                            }
                         }
-                        Section("Vis Km:"){
-                            Text("st").font(.subheadline).fontWeight(.light)
+                        Section(){
+                            HStack(){
+                                Text("Tempature in Cº:").fontWeight(.semibold)
+                                Text("st").font(.subheadline).fontWeight(.light)
+                            }
                         }
-                        Section("Condition:"){
-                            Text("st").font(.subheadline).fontWeight(.light)
+                        Section(){
+                            HStack(){
+                                Text("Tempature in Cº:").fontWeight(.semibold)
+                                Text("").font(.subheadline).fontWeight(.light)
+                            }
                         }
                     }
                     Section("Location Data"){
-                        Section("Feels like Cº:"){
-                            Text("st").font(.subheadline).fontWeight(.light)
+                        Section(){
+                            HStack(){
+                                Text("Tempature in Cº:").fontWeight(.semibold)
+                                Text("st").font(.subheadline).fontWeight(.light)
+                            }
                         }
-                        Section("Wind Kph:"){
-                            Text("st").font(.subheadline).fontWeight(.light)
+                        Section(){
+                            HStack(){
+                                Text("Tempature in Cº:").fontWeight(.semibold)
+                                Text("st").font(.subheadline).fontWeight(.light)
+                            }
                         }
-                        Section("Wind Direction:"){
-                            Text("st").font(.subheadline).fontWeight(.light)
+                        Section(){
+                            HStack(){
+                                Text("Tempature in Cº:").fontWeight(.semibold)
+                                Text("st").font(.subheadline).fontWeight(.light)
+                            }
                         }
                     }
-                    
                     
                 }.listStyle(.automatic)
                 
---
+++ AdolfoWeather/ViewModel/WeatherViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherViewModel.swift
+//  AdolfoWeather
+//
+//  Created by David Romero on 2024-11-10.
+//
+
+import Foundation
+
+class WeatherViewModel: ObservableObject{
+    
+}
